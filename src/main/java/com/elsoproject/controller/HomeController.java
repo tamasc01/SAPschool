a8c15fb4b6088c102426b82037b851f6f451821a
tamasc01 <tamas.haaz.92@gmail.com>
2019-12-31 14:05:31 +0100

    My static site, first commit

---
+++ src/main/java/com/elsoproject/controller/HomeController.java
@@ -0,0 +1,47 @@
+package com.elsoproject.controller;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String homepage(Model model, Locale locale) {		
+		System.out.println(String.format("Request received. Language: %s, Country: %s %n", locale.getLanguage(),
+				locale.getDisplayCountry()));
+		return "homepage";
+	}
+		
+	@RequestMapping("/bevezetes")
+	public String bevezetes(Model model) {			
+		return "bevezetes";
+	}
+	
+	@RequestMapping("/sapbusinessuite")
+	public String sapbusinesssuite(Model model) {			
+		return "sapbusinessuite";
+	}
+	
+	@RequestMapping("/{title}")
+	public String searchForUser(@PathVariable(value = "title") String title, Model model) throws Exception {
+		if (title == null)
+			throw new Exception("Nincs ilyen címmel oldalunk");		
+		return "error";
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(HttpServletRequest rA, Exception ex, Model model) {
+		model.addAttribute("errorMessage", ex.getMessage());
+		return "exceptionHandler";
+	}
+	
+	
+}
