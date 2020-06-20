4a4a188bd21bb3107f8a645c00fec63790af1d74
tamasc01 <tamas.haaz.92@gmail.com>
2020-06-20 14:50:42 +0200

    war file

--- src/main/java/com/elsoproject/ElsoProjectApplication.java
+++ src/main/java/com/elsoproject/ElsoProjectApplication.java
@@ -2,13 +2,31 @@ package com.elsoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 
+
+@EnableConfigurationProperties
+@PropertySources({
+    @PropertySource(value = "file:${appConf}", ignoreResourceNotFound = true)
+})
 @SpringBootApplication
-public class ElsoProjectApplication {
+public class ElsoProjectApplication extends org.springframework.boot.web.servlet.support.SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElsoProjectApplication.class, args);
 	}
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(ElsoProjectApplication.class);
+    }
 
+	
 }
